7532a95319b2e28fcf9b98aef5ad07e07067890c
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-11-23 02:14:37 +0530

    feat: add-navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import { NavbarComponent } from "@/components/NavbarComponent";
 import Image from "next/image";
 
 export default function Home() {
   return (
-  <p>hello</p>
+ <NavbarComponent/>
   );
 }
