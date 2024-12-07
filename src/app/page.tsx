15ad73b5f828ccc5be282849fda3841223eaa089
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-12-07 18:06:00 +0530

    chore: nextui-init-start-with-nav-component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { AuroraBackground } from "@/components/ui/aurora-background";
 export default function Home() {
   return (
     <>
-      <NavbarComponent />
+      {/* <NavbarComponent /> */}
       <AuroraBackgroundDemo/>
       <AboutSection />
       
